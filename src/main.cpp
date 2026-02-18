b532e48a19d4777f7344ef72276712330aa5c380
Tuomo Björk <tuomo.bjork@tbk.fi>
2026-02-18 14:15:27 +0200

    update: src/main.cpp (freertos test via blinkies)

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,42 @@
-#include <stdio.h>
+#include "FreeRTOS.h"
+#include "task.h"
+#include "pico/stdlib.h"
 
-int main(void) {
-	return 0;
+#include "hardware/timer.h"
+extern "C" {
+uint32_t read_runtime_ctr(void) {
+    return timer_hw->timerawl;
+}
+}
+
+struct led_params{
+    uint pin;
+    uint delay;
+};
+
+void blink_task(void *param)
+{
+    led_params *lpr = (led_params *) param;
+    const uint led_pin = lpr->pin;
+    const uint delay = lpr->delay;
+
+    gpio_init(led_pin);
+    gpio_set_dir(led_pin, GPIO_OUT);
+
+    while (true) {
+        gpio_put(led_pin, 1); vTaskDelay(delay);
+        gpio_put(led_pin, 0); vTaskDelay(delay);
+    }
+}
+
+int main() {
+    static led_params lp1 = { .pin = 20, .delay = 250 };
+    static led_params lp2 = { .pin = 21, .delay = 500 };
+
+    xTaskCreate(blink_task, "LED_1", 256, (void *) &lp1, tskIDLE_PRIORITY + 1, NULL);
+    xTaskCreate(blink_task, "LED_2", 256, (void *) &lp2, tskIDLE_PRIORITY + 1, NULL);
+
+    vTaskStartScheduler();
+
+    while(true);
 }
